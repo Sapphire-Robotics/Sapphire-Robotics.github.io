c4ce822042a6b3de5334b97aeedf7501c9ce630a
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2025-07-03 18:11:43 -0400

    Add content

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,16 @@ export default function Page() {
             <PageTitle>Placeholder Robotics Club</PageTitle>
             <PageContent>
                 <p>
-                  We are a robotics club located in Apex, North Carolina. This fall, we will be running two FLL teams.
+                    We help youth develop collaboration and communication skills by having them solve challenging engineering problems together.
+                    <br />We participate in our local FIRST LEGO League Challenge competitions in North Carolina.
+                </p>
+                <h2>
+                    What is FIRST LEGO League?
+                </h2>
+                <p>
+                    "FIRST® LEGO® League introduces science, technology, engineering, and math (STEM) to children ages 4-16 through fun, exciting hands-on learning. Participants gain real-world problem solving experience through a guided, global robotics program, helping today's students and teachers build a better future together. Our three divisions inspire youth to experiment and grow their critical thinking, coding, and design skills through hands-on STEM learning and robotics." (Source: <a href="https://www.firstlegoleague.org/about" style={{
+                        color: "#fadbff"
+                    }}>FIRST LEGO League</a>
                 </p>
                 <Image src="/placeholder_picture.png" width={1280} height={1280} alt="Placeholder image" style={{
                   width: "350px",
--- src/app/support/page.js
+++ src/app/support/page.js
@@ -7,8 +7,7 @@ export default function Page() {
             <PageTitle>Support</PageTitle>
             <PageContent>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-                    <br /><br />Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                    Thank you to our sponsors.
                 </p>
             </PageContent>
         </>
